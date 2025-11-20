02f99639ddbc41a9a77e1eee0fd17d8792747e48
BKIT_Solutions <baburao1000cr@gmail.com>
2025-11-20 12:30:13 +0530

    Update DBConnection.java

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/insta_blog";
     private static final String USER = "root";
-    private static final String PASS = "ijustDh53@";
+    private static final String PASS = "==Mysql_Password==";
 
     public static Connection getConnection() throws SQLException {
         try {
